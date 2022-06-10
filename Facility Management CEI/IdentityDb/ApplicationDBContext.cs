b5b005af435f98be554b7080faf12a195f638e80
Mohammed Jamal Mahmoud <mohammed.j.mahmoud@gmail.com>
2022-06-10 19:48:44 +0200

    dd

---
+++ Facility Management CEI/IdentityDb/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Facility_Management_CEI.IdentityDb
+{
+    public class ApplicationDBContext : IdentityDbContext<IdentityUser, IdentityRole,string>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> option)
+            :base(option)
+        {
+
+        }
+    }
+}
