a1171dd62de6e8078d62f9be1065b55216ed109a
Mohammed Jamal Mahmoud <mohammed.j.mahmoud@gmail.com>
2022-06-10 19:09:06 +0200

    dd

---
+++ Facility Management CEI/Models/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using API.Enums;
+
+namespace Facility_Management_CEI.Models
+{
+    public class RegisterViewModel
+    {
+        //public int Id { get; set; } //the id is auto generated 
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public UserType Type { get; set; }
+        public int? BuildingId { get; set; }//test the null
+        public int? SuperId { get; set; }//test the null
+    }
+}
