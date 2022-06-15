3d7fc047000a285cffc113141dfdc18793c38bef
Ahmad El-Banna <ahmed_elbanna159@yahoo.com>
2022-06-16 00:47:22 +0200

    ahmad kamaaaaaaaaaaaal new

--- Facility Management CEI/Controllers/TaskPagesController.cs
+++ Facility Management CEI/Controllers/TaskPagesController.cs
@@ -58,6 +58,120 @@ namespace Skote.Controllers
             ViewBag.ListOfTasks = tasks;
             return View();
         }
+        //show details by id we may call is search
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            var task = await _Context.Tasks
+                .Include(t => t.AssignedBy)
+                .Include(t => t.AssignedTo)
+                .Include(t => t.CreatedBy)
+                .Include(t => t.Incident)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _Context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            ViewData["AssignedById"] = new SelectList(_Context.AppUsers, "Id", "LogUserId", task.AssignedById);
+            ViewData["AssignedToId"] = new SelectList(_Context.AppUsers, "Id", "LogUserId", task.AssignedToId);
+            ViewData["CreatedById"] = new SelectList(_Context.AppUsers, "Id", "LogUserId", task.CreatedById);
+            ViewData["IncidentId"] = new SelectList(_Context.Incidents, "Id", "Id", task.IncidentId);
+            return View(task);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Type,Description,Status,Priority,Cost,FixingTime,CreatedById,IncidentId,AssignedToId,AssignedById")] Task task)
+        {
+            if (id != task.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _Context.Update(task);
+                    await _Context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskExists(task.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(TaskList));
+            }
+            ViewData["AssignedById"] = new SelectList(_Context.AppUsers, "Id", "LogUserId", task.AssignedById);
+            ViewData["AssignedToId"] = new SelectList(_Context.AppUsers, "Id", "LogUserId", task.AssignedToId);
+            ViewData["CreatedById"] = new SelectList(_Context.AppUsers, "Id", "LogUserId", task.CreatedById);
+            ViewData["IncidentId"] = new SelectList(_Context.Incidents, "Id", "Id", task.IncidentId);
+            return View(task);
+        }
+        //delete func..it's important to create this before using any http verb as it works like get and then we use post verb in the next function 
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _Context.Tasks
+                .Include(t => t.AssignedBy)
+                .Include(t => t.AssignedTo)
+                .Include(t => t.CreatedBy)
+                .Include(t => t.Incident)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var task = await _Context.Tasks.FindAsync(id);
+            _Context.Tasks.Remove(task);
+            await _Context.SaveChangesAsync();
+            return RedirectToAction(nameof(TaskList));
+        }
+
+
+
+
+        private bool TaskExists(int id)
+        {
+            return _Context.Tasks.Any(e => e.Id == id);
+        }
     }
 }
