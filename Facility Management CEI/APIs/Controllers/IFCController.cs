8b521facae183822d421486f567d6f2ddb1f4308
Ramy Adel <81263757+Ramyadel2300@users.noreply.github.com>
2022-06-17 20:49:56 +0200

    nothing to add

--- Facility Management CEI/APIs/Controllers/IFCController.cs
+++ Facility Management CEI/APIs/Controllers/IFCController.cs
@@ -16,6 +16,7 @@ using Xbim.Ifc4.ProductExtension;
 using Xbim.Ifc4.SharedBldgElements;
 using Xbim.Ifc4.SharedBldgServiceElements;
 
+
 namespace API.Controllers
 {
     [Route("api/[controller]")]//the route that the request and response will take 
