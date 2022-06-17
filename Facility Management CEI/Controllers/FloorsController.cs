264b8cdbc9d623369fa7477b49de4b14fb7e057a
Ahmad El-Banna <ahmed_elbanna159@yahoo.com>
2022-06-17 18:57:43 +0200

    building components with views and controllers aslo added some changes to Tasks

---
+++ Facility Management CEI/Controllers/FloorsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+using Facility_Management_CEI.IdentityDb;
+
+namespace Facility_Management_CEI.Controllers
+{
+    public class FloorsController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public FloorsController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Floors
+        public async Task<IActionResult> Index()
+        {
+            var applicationDBContext = _context.Floors.Include(f => f.Building);
+            return View(await applicationDBContext.ToListAsync());
+        }
+
+        // GET: Floors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var floor = await _context.Floors
+                .Include(f => f.Building)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (floor == null)
+            {
+                return NotFound();
+            }
+
+            return View(floor);
+        }
+
+    }
+}
